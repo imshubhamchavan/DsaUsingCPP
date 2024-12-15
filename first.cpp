1b7e32ff12eab3dcf2709ef4ed3b9652c892e843
imshubhamchavan <imshubhamchavan@gmail.com>
2024-12-15 17:23:38 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter value for a: ";
+    cin>>a;
+
+    cout<<"enter value for b: ";
+    cin>>b;
+
+    int sum = a+b;
+    cout<<"sum of a and b is "<<sum;
+    return 0;
+}
---
+++ patterns/pattern1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+ int main(){
+    int n=6;
+    for(int i=1; i<=n; i++){
+        cout<<"\n";
+        for(int j=1; j<=n; j++){
+            cout<<j<<"";
+        }
+    }
+ }
