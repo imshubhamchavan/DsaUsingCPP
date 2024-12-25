e05ebeff7b503d092d4e77e1d5335d9634b84c18
imshubhamchavan <imshubhamchavan@gmail.com>
2024-12-25 18:18:54 +0530

    solved hat problem using c++

--- practice/hat.cpp
+++ practice/hat.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 int main(){
     string a;
-    string b="white";
-    string c="black";
+    string b;
+    string c;
 
-    if(b=="white" && c=="black"){
-        cout<<"a is wearing black";
-    }if(b=="black" && c=="white"){
+    cout<<"you can choose either black or white.\n";
+    cout<<"enter the hat color for b:";
+    cin>>b;
+    
+    cout<<"enter the hat color for c:";
+    cin>>c;
+
+    if(b=="white" && c=="black" || b=="black" && c=="white"){
+        cout<<"a either wearing black hat or white hat ";
+    }else if(b=="white" && c=="white"){
         cout<<"a is wearing black ";
     }else{
         cout<<" c is wearing black";
---
+++ practice/hat.js
@@ -0,0 +1,9 @@
+const hatproblem = ()=>{
+    let a;
+    let b="white";
+    let c="black";
+
+    if(b=="white" && c=="black"){
+        console.log("a either wearing black hat or white hat ");
+    }if (){}
+}
