4089c7c2eb7cd013c9b73ab4b2daa768785b3dac
imshubhamchavan <imshubhamchavan@gmail.com>
2024-12-25 18:26:34 +0530

    solved pattern1 using javascript

--- patterns/pattern.js
+++ patterns/pattern.js
@@ -1,7 +1,8 @@
-for (let i = 1; i <= 6; i++) {
+let n=7;
+for (let i = 1; i <= n; i++) {
     console.log(""); 
     let row = "";
-    for (let j = 1; j <= 6; j++) {
+    for (let j = 1; j <= n; j++) {
       row += j; 
     }
     console.log(row); 
